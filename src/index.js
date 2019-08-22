9b8037a84cbe105a6f7bfee822e9f6635e4af7c2
hugginssd <hugginsmafigu@gmail.com>
2019-08-22 20:34:43 +0200

    api key

--- src/index.js
+++ src/index.js
@@ -5,16 +5,28 @@ import * as serviceWorker from './serviceWorker';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
 import LoginComponent from './login/login';
 import SignupComponent from './signup/signup';
+import CastVoteComponent from './castvote/castvote';
 import DashboardComponent from './dashboard/dashboard';
 
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyChUmimdAXTxkDpj9UIt0zAm7D_FDvB-zU",
+    authDomain: "chat-app-21680.firebaseapp.com",
+    databaseURL: "https://chat-app-21680.firebaseio.com",
+    projectId: "chat-app-21680",
+    storageBucket: "chat-app-21680.appspot.com",
+    messagingSenderId: "347502776442",
+    appId: "1:347502776442:web:e909df12d5137eaf"
+});
 
 const routing = ( 
         <Router >
             <div id = "routing-container" >
                 <Route path = '/login' component = { LoginComponent } ></Route> 
                 <Route path = '/signup' component = { SignupComponent } ></Route> 
-                <Route path = '/castvote' component = { CastVoteComponent } ></Route> 
-                <Route path = '/signup' component = { SignupComponent } ></Route> 
+                <Route path = '/castvote' component = { CastVoteComponent } ></Route>
                 <Route path = '/dashboard' component = { DashboardComponent }></Route> 
             </div>  
         </Router>
